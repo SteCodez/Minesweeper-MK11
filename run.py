b9af5cccec8eb8ea054a0d23ba0825360e19ff3d
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-09-29 15:38:10 +0100

    cutting out menu to see if board prints

--- run.py
+++ run.py
@@ -1,32 +1,3 @@
-import random, os, time
-from typing import no_type_check_decorator
-#Introduction
-print()
-print('A Game that starts with chance')
-print('And ends with wits!')
-print('Good luck!')
-print()
-
-def reset():
-    print('''
-MAIN MENU
-=========
--> For instructions on how to play, type 'I'
--> To play immediately, type 'P'
-''')
-    
-    choice = input('Type here: ').upper
-    
-    if choice == 'I':
-        os.system('clear')
-        
-        print(open('instructions.txt', 'r').read())
-        
-        input('Press [enter] when ready to play. ')
-        
-    elif choice != 'p':
-        os.system('clear')
-        reset()
 
 def field_layout(): #Function for printing the layout by diplaying a grid in each iteration
     
