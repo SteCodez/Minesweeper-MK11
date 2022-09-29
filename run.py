e3dcaccdcc13ce145c1a650630892f5d50dcf5a6
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-09-29 02:51:12 +0100

    This adds a play option or an option to look at the instructions.

--- run.py
+++ run.py
@@ -1,1 +1,29 @@
 import random, os
+#Introduction
+print()
+print('A Game that starts with chance')
+print('And ends with wits!')
+print('Good luck!')
+print()
+
+def reset():
+    print('''
+MAIN MENU
+=========
+
+-> For instructions on how to play, type 'I'
+-> To play immediately, type 'P'
+''')
+    
+    choice = input('Type here: ').upper
+    
+    if choice == 'I':
+        os.system('clear')
+        
+        print(open('instructions.txt', 'r').read())
+        
+        input('Press [enter] when ready to play. ')
+        
+    elif choice != 'p':
+        os.system('clear')
+        reset()
